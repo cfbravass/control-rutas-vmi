6389b212d8841eff0e1ae76b0867c09845aa11d4
Santiago Agudelo <sagudelo1200@gmail.com>
2023-07-10 08:55:54 -0500

    Prueba modal Mapa backdrop

--- src/views/Rutas.js
+++ src/views/Rutas.js
@@ -16,21 +16,14 @@ function Rutas() {
     nombre: '',
     fecha: '',
   })
-  const [showModal, setShowModal] = useState(false)
 
   const abrirMapa = (latitude, longitude, nombre, fecha) => {
-    console.log(latitude, longitude, nombre, fecha)
     setInfoMapa({
       latitude,
       longitude,
       nombre,
       fecha,
     })
-    setShowModal(true)
-  }
-
-  const closeModal = () => {
-    setShowModal(false)
   }
 
   const ordenarFechas = (fechas) => {
@@ -71,158 +64,166 @@ function Rutas() {
   }
 
   return (
-    <div className='container'>
-      <h1 className='text-center'>Mis Rutas Programadas</h1>
-      {rutasUsuario.map((ruta) => (
-        <div className='row mx-2' key={ruta.id}>
-          <div className='text-center p-2'>ID Ruta: {ruta.id}</div>
-          {ordenarFechas(Object.keys(ruta.locaciones)).map((fecha) => {
-            const [dia, mes, anio] = fecha.split('-')
-            const date = new Date(anio, mes - 1, dia)
-            let formattedFecha = date.toLocaleDateString('es-ES', {
-              weekday: 'long',
-              day: 'numeric',
-              month: 'numeric',
-              year: 'numeric',
-            })
+    <>
+      <div className='container'>
+        <h1 className='text-center'>Mis Rutas Programadas</h1>
 
-            formattedFecha =
-              formattedFecha.charAt(0).toUpperCase() + formattedFecha.slice(1)
+        {rutasUsuario.map((ruta) => (
+          <div className='row mx-2' key={ruta.id}>
+            <div className='text-center p-2'>ID Ruta: {ruta.id}</div>
+            {ordenarFechas(Object.keys(ruta.locaciones)).map((fecha) => {
+              const [dia, mes, anio] = fecha.split('-')
+              const date = new Date(anio, mes - 1, dia)
+              let formattedFecha = date.toLocaleDateString('es-ES', {
+                weekday: 'long',
+                day: 'numeric',
+                month: 'numeric',
+                year: 'numeric',
+              })
 
-            return (
-              <div
-                className='col-12 col-lg-2 text-center mb-3 border rounded bg-light py-2'
-                key={fecha}
-              >
-                <div className='row mb-4'>
-                  <div className='col'>
-                    <div>{formattedFecha}</div>
-                    {ruta.locaciones[fecha].almacenesProgramados && (
-                      <>
-                        <p className='m-0 text-primary'>
-                          Almacenes Programados
-                        </p>
-                        <div>
-                          {ruta.locaciones[fecha].almacenesProgramados.map(
-                            (nombreAlmacen) => (
-                              <div key={`${nombreAlmacen}+${fecha}`}>
-                                {nombreAlmacen}
-                                <button
-                                  onClick={() =>
-                                    handleMarcarLlegada(
-                                      ruta.id,
-                                      fecha,
-                                      nombreAlmacen
-                                    )
-                                  }
-                                >
-                                  Marcar llegada
-                                </button>
-                              </div>
-                            )
-                          )}
-                        </div>
-                      </>
-                    )}
-                    {ruta.locaciones[fecha].almacenesVisitados !== {} && (
-                      <>
-                        <p className='m-0 text-info'>Almacenes Visitados</p>
-                        <div>
-                          {Object.entries(
-                            ruta.locaciones[fecha].almacenesVisitados
-                          ).map(([nombreAlmacen, almacenData]) => (
-                            <div key={nombreAlmacen}>
-                              {nombreAlmacen}
-                              <br />
-                              Hora de visita:{' '}
-                              {almacenData.horaVisita && (
-                                <>
-                                  {typeof almacenData.horaVisita.toDate ===
-                                  'function'
-                                    ? almacenData.horaVisita
-                                        .toDate()
-                                        .toLocaleString('es-ES')
-                                    : almacenData.horaVisita.toLocaleString(
-                                        'es-ES'
-                                      )}
-                                </>
-                              )}
-                              <br />
-                              Ubicación:{' '}
-                              {almacenData.ubicacion && (
-                                <>
-                                  {almacenData.ubicacion.latitude},{' '}
-                                  {almacenData.ubicacion.longitude}
+              formattedFecha =
+                formattedFecha.charAt(0).toUpperCase() + formattedFecha.slice(1)
+
+              return (
+                <div
+                  className='col-12 col-lg-2 text-center mb-3 border rounded bg-light py-2'
+                  key={fecha}
+                >
+                  <div className='row mb-4'>
+                    <div className='col'>
+                      <div>{formattedFecha}</div>
+                      {ruta.locaciones[fecha].almacenesProgramados && (
+                        <>
+                          <p className='m-0 text-primary'>
+                            Almacenes Programados
+                          </p>
+                          <div>
+                            {ruta.locaciones[fecha].almacenesProgramados.map(
+                              (nombreAlmacen) => (
+                                <div key={`${nombreAlmacen}+${fecha}`}>
+                                  {nombreAlmacen}
                                   <button
                                     onClick={() =>
-                                      abrirMapa(
-                                        almacenData.ubicacion.latitude,
-                                        almacenData.ubicacion.longitude,
-                                        nombreAlmacen,
-                                        typeof almacenData.horaVisita.toDate ===
-                                          'function'
-                                          ? almacenData.horaVisita
-                                              .toDate()
-                                              .toLocaleString('es-ES')
-                                          : almacenData.horaVisita.toLocaleString(
-                                              'es-ES'
-                                            )
+                                      handleMarcarLlegada(
+                                        ruta.id,
+                                        fecha,
+                                        nombreAlmacen
                                       )
                                     }
                                   >
-                                    Ver
+                                    Marcar llegada
                                   </button>
-                                </>
-                              )}
-                            </div>
-                          ))}
-                        </div>
-                      </>
-                    )}
+                                </div>
+                              )
+                            )}
+                          </div>
+                        </>
+                      )}
+                      {ruta.locaciones[fecha].almacenesVisitados !== {} && (
+                        <>
+                          <p className='m-0 text-info'>Almacenes Visitados</p>
+                          <div>
+                            {Object.entries(
+                              ruta.locaciones[fecha].almacenesVisitados
+                            ).map(([nombreAlmacen, almacenData]) => (
+                              <div key={nombreAlmacen}>
+                                {nombreAlmacen}
+                                <br />
+                                Hora de visita:{' '}
+                                {almacenData.horaVisita && (
+                                  <>
+                                    {typeof almacenData.horaVisita.toDate ===
+                                    'function'
+                                      ? almacenData.horaVisita
+                                          .toDate()
+                                          .toLocaleString('es-ES')
+                                      : almacenData.horaVisita.toLocaleString(
+                                          'es-ES'
+                                        )}
+                                  </>
+                                )}
+                                <br />
+                                Ubicación:{' '}
+                                {almacenData.ubicacion && (
+                                  <>
+                                    {almacenData.ubicacion.latitude},{' '}
+                                    {almacenData.ubicacion.longitude}
+                                    <button
+                                      type='button'
+                                      data-bs-toggle='modal'
+                                      data-bs-target='#modalMapa'
+                                      onClick={() =>
+                                        abrirMapa(
+                                          almacenData.ubicacion.latitude,
+                                          almacenData.ubicacion.longitude,
+                                          nombreAlmacen,
+                                          typeof almacenData.horaVisita
+                                            .toDate === 'function'
+                                            ? almacenData.horaVisita
+                                                .toDate()
+                                                .toLocaleString('es-ES')
+                                            : almacenData.horaVisita.toLocaleString(
+                                                'es-ES'
+                                              )
+                                        )
+                                      }
+                                    >
+                                      Ver
+                                    </button>
+                                  </>
+                                )}
+                              </div>
+                            ))}
+                          </div>
+                        </>
+                      )}
+                    </div>
                   </div>
                 </div>
-              </div>
-            )
-          })}
-        </div>
-      ))}
+              )
+            })}
+          </div>
+        ))}
+      </div>
 
-      {showModal && (
-        <div
-          className='modal fade show'
-          tabIndex='-1'
-          style={{ display: 'block' }}
-        >
-          <div className='modal-dialog modal-dialog-centered modal-lg'>
-            <div className='modal-content'>
-              <div className='modal-header'>
-                <h1 className='modal-title fs-5'>Mapa</h1>
-                <button
-                  type='button'
-                  className='btn-close'
-                  data-bs-dismiss='modal'
-                  aria-label='Close'
-                  onClick={closeModal}
-                ></button>
-              </div>
-              <div className='modal-body'>
-                <Mapa infoMapa={infoMapa} />
-              </div>
-              <div className='modal-footer'>
-                <button
-                  type='button'
-                  className='btn btn-secondary'
-                  data-bs-dismiss='modal'
-                  onClick={closeModal}
-                >
-                  Cerrar
-                </button>
-              </div>
+      <Mapa infoMapa={infoMapa} />
+
+      <div
+        className='modal fade'
+        id='modalMapa'
+        tabIndex='-1'
+        aria-labelledby='modalMapaLabel'
+        aria-hidden='true'
+      >
+        <div className='modal-dialog'>
+          <div className='modal-content'>
+            <div className='modal-header'>
+              <h1 className='modal-title fs-5' id='modalMapaLabel'>
+                Mapa
+              </h1>
+              <button
+                type='button'
+                className='btn-close'
+                data-bs-dismiss='modal'
+                aria-label='Close'
+              ></button>
+            </div>
+            <div className='modal-body d-flex align-items-center justify-content-center'>
+              <Mapa infoMapa={infoMapa} />
+            </div>
+            <div className='modal-footer'>
+              <button
+                type='button'
+                className='btn btn-secondary'
+                data-bs-dismiss='modal'
+              >
+                Cerrar
+              </button>
             </div>
           </div>
         </div>
-      )}
-    </div>
+      </div>
+    </>
   )
 }
 
